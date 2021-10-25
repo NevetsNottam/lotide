416c48d6b9e2d311888d774c11a6740bfc464bbb
steven <stevenmatton100@gmail.com>
2021-10-25 16:50:04 -0400

    created a function that compares two inputs and determines if they are equal or not

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion Passed:  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🍎 🍎 🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(1, 3);
