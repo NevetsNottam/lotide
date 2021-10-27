de8eeba4f47caae7f0484c2e1ad1b2c8e21396c9
steven <stevenmatton100@gmail.com>
2021-10-27 00:36:10 -0400

    commiting all changes

--- tail.js
+++ tail.js
@@ -1,5 +1,3 @@
-
-
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -22,3 +20,5 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+
