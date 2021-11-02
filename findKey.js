618b9d5e2acf25de4b9f2e8acc935d70317e9aba
steven <stevenmatton100@gmail.com>
2021-11-01 22:59:04 -0400

    refactored findKey to put test cases in its own test file and to use mocha and chai

--- findKey.js
+++ findKey.js
@@ -1,13 +1,5 @@
 
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅  Assertion Passed:  ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🍎 🍎 🍎  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKey = function(obj, callback) {
   
   let newArr = Object.keys(obj);
@@ -28,12 +20,4 @@ const findKey = function(obj, callback) {
 // iterate through the object and return the first key for which the call back returns truthy
 // if no key is found, return undefined
 
-//tests
-console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 1), "Blue Hill")); // => "noma"
+module.exports = findKey;
